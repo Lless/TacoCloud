4d3422e52ab7c24401189fde281ce49a22a26b9a
Igor Shangin <igorsan1@mail.ru>
2020-03-31 02:44:16 +0300

    chapter 2.3, validation

--- src/main/java/com/github/lless/tacos/DesignTacoController.java
+++ src/main/java/com/github/lless/tacos/DesignTacoController.java
@@ -4,11 +4,12 @@ import com.github.lless.tacos.Ingredient.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,7 +44,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@ModelAttribute("design") Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: " + design);
 
         return "redirect:/orders/current";
--- src/main/java/com/github/lless/tacos/OrderController.java
+++ src/main/java/com/github/lless/tacos/OrderController.java
@@ -3,9 +3,12 @@ package com.github.lless.tacos;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -14,11 +17,15 @@ public class OrderController {
     @GetMapping("/current")
     public String orderFrom(Model model){
         model.addAttribute("order", new Order());
-        return "orderFrom";
+        return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("order submitted: " + order);
         return "redirect:/";
     }
